f21c1f74eca1a4417bebc2a0fed03efa40538485
andyl <andy@r210.com>
2017-03-31 14:48:12 -0700

    Update capistrano output

--- config/deploy/base.rb
+++ config/deploy/base.rb
@@ -4,7 +4,7 @@ puts ' CAP CONFIG BASE '.center(70,'-')
 
 set :application,    'lica'
 set :log_level,      :error     # use :error, :warn, :info, or :debug
-set :command_output, false
+set :format_options, command_output: false
 
 set :deploy_to,   -> { "/home/#{fetch(:user)}/a/#{fetch(:application).downcase}" }
 

070bb58e8d3183ba4502e2ca3e600d857d43b573
andyl <andy@r210.com>
2017-04-03 15:04:40 -0700

    Rename log view script

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
 # Version of your assets, change this if you want to expire all your assets.
-Rails.application.config.assets.version = '1.0'
+Rails.application.config.assets.version = '1.1'
 
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
@@ -15,7 +15,6 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 
 js_prefix = 'app/assets/javascripts/'
 Rails.application.config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
-Rails.application.config.assets.precompile += %w( ztst/all_chat.js )
 Rails.application.config.assets.precompile += Dir.glob("#{js_prefix}**/all_*.js").map {|x| x.gsub(js_prefix, '')}
 Rails.application.config.assets.precompile += Dir.glob("#{js_prefix}*.js.coffee").map {|x| x.gsub(js_prefix, '').gsub('.coffee','')}
 Rails.application.config.assets.paths << Rails.root.join('vendor/assets/javascripts')

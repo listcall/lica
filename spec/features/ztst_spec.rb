e153b5a93800e58f9ace758d0d2f9a8b61ae3e9e
andyl <andy@r210.com>
2017-04-07 11:54:53 -0700

    Update webpacker config

--- spec/features/ztst_spec.rb
+++ spec/features/ztst_spec.rb
@@ -10,8 +10,10 @@ describe 'Ztst', :js do
   end
 
   context 'as a registered user' do
-    puts ZTST_PAGES #
-    (ZTST_PAGES).each do |page_name|
+    # (ZTST_PAGES).each do |page_name|
+    dong = %w(index pack2)
+    dong.each do |page_name|
+      puts page_name
       it "renders #{page_name}" do
         tst_path = "/ztst/#{page_name}"
         visit tst_path

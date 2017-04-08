030111bdf9fdd04c75cdec79f7a24d08c95baf6a
andyl <andy@r210.com>
2017-04-08 14:56:27 -0700

    Update spec runner

---
+++ app/javascript/tst/src1.js
@@ -0,0 +1,6 @@
+function sum(a,b) { return a + b; }
+
+function bing(b = "what") { return b; }
+
+module.exports = {sum: sum, bing: bing}
+
---
+++ app/javascript/tst/src2.coffee
@@ -0,0 +1,6 @@
+sum = (a,b) => a + b
+
+bing = (b = "what") => b
+
+module.exports = {sum: sum, bing: bing}
+

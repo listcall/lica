e11f92c7eb8c8fc2324045d8fa1b8c0dcb063341
andyl <andy@r210.com>
2017-04-12 13:44:44 -0700

    Remove Draper code

--- app/decorators/event_period_decorator.rb
+++ app/decorators/event_period_decorator.rb
@@ -1,62 +1,62 @@
-class EventPeriodDecorator < Draper::Decorator
-  delegate_all
-
-  def period_cell
-    x_klas = context[:parent_row] ? 'parentCell' : 'childCell'
-    posn   = context[:parent_row] ? position : parent.try(:position)
-    <<-ERB
-      <span id="#{id}_#{position}" data-posn="#{posn}" class="periodCell #{x_klas}">
-        #{period_label}#{start_label}
-        <div class="participantCell">
-          #{participant_widget(participants.count)}
-          #{period_cell_delete(participants.count)}
-        </div>
-      </span>
-    ERB
-  end
-
-  def start_date
-    first_participant_start || prior_period_start
-  end
-
-  private
-
-  def participant_widget(count)
-    "<span style='color: black;' class='badge periodBadge'><i class='fa fa-group'></i> #{count}</span>"
-  end
-
-  def period_label
-    return "P#{position} " if context[:parent_row]
-    return '' if parent.blank?
-    "P#{parent.position} "
-  end
-
-  def period_cell_delete(count)
-    return '' if count != 0
-    return '' if self.event.periods.count < 2
-    "<i data-perid='#{self.id}' class='fa fa-times-circle perDel'></i>"
-  end
-
-  # ----- start date calculation and display -----
-
-  def start_label
-    start_date.try(:strftime, '%b%-d')
-  end
-
-  def first_participant_start
-    participants.pluck(:departed_at, :signed_in_at).flatten.select{|x| x.present?}.min
-  end
-
-  def prior_period
-    return 0 if position.nil?
-    obj = event.periods.find_by_position(position - 1)
-    return nil unless obj.present?
-    EventPeriodDecorator.new(obj)
-  end
-
-  def prior_period_start
-    return event.start if position.nil? || position < 2
-    prior_period.try(:start_date).try(:+, 1.day)
-  end
-
-end
+# class EventPeriodDecorator < Draper::Decorator
+#   delegate_all
+#
+#   def period_cell
+#     x_klas = context[:parent_row] ? 'parentCell' : 'childCell'
+#     posn   = context[:parent_row] ? position : parent.try(:position)
+#     <<-ERB
+#       <span id="#{id}_#{position}" data-posn="#{posn}" class="periodCell #{x_klas}">
+#         #{period_label}#{start_label}
+#         <div class="participantCell">
+#           #{participant_widget(participants.count)}
+#           #{period_cell_delete(participants.count)}
+#         </div>
+#       </span>
+#     ERB
+#   end
+#
+#   def start_date
+#     first_participant_start || prior_period_start
+#   end
+#
+#   private
+#
+#   def participant_widget(count)
+#     "<span style='color: black;' class='badge periodBadge'><i class='fa fa-group'></i> #{count}</span>"
+#   end
+#
+#   def period_label
+#     return "P#{position} " if context[:parent_row]
+#     return '' if parent.blank?
+#     "P#{parent.position} "
+#   end
+#
+#   def period_cell_delete(count)
+#     return '' if count != 0
+#     return '' if self.event.periods.count < 2
+#     "<i data-perid='#{self.id}' class='fa fa-times-circle perDel'></i>"
+#   end
+#
+#   # ----- start date calculation and display -----
+#
+#   def start_label
+#     start_date.try(:strftime, '%b%-d')
+#   end
+#
+#   def first_participant_start
+#     participants.pluck(:departed_at, :signed_in_at).flatten.select{|x| x.present?}.min
+#   end
+#
+#   def prior_period
+#     return 0 if position.nil?
+#     obj = event.periods.find_by_position(position - 1)
+#     return nil unless obj.present?
+#     EventPeriodDecorator.new(obj)
+#   end
+#
+#   def prior_period_start
+#     return event.start if position.nil? || position < 2
+#     prior_period.try(:start_date).try(:+, 1.day)
+#   end
+#
+# end
--- app/decorators/member_cert_decorator.rb
+++ app/decorators/member_cert_decorator.rb
@@ -1,48 +1,48 @@
-class MemberCertDecorator < Draper::Decorator
-  delegate_all
-
-  def cert_cells
-    types = h.current_team.qual_ctypes
-    return ''
-    # types.to_a.reduce("") do |acc, obj|
-      #certs = membership_certs.where(typ: obj.id)
-      # certs = membership_certs.where('typ ilike ?', obj.id)
-      # acc + cert_cell(certs, obj)
-    # end
-  end
-
-  # def cert_cell(certs, type_def)
-  #   val   = req_val(certs)
-  #   klas  = req_class(certs, type_def)
-  #   "<td class='#{klas}'><small>#{val}</small></td>"
-  # end
-
-  def req_val(certs)
-    return '' if certs.blank?
-    aster  = certs.length > 1 ? ' *' : ''
-    req_link(certs.first, aster)
-  end
-
-  def req_link(cert, aster)
-    label = "#{cert.title}#{aster}"
-    link  = cert.attachment_file_name.present? ? cert.attachment.url : ''
-    return label if link.empty?
-    "<a href='#{link}'>#{label}</a>"
-  end
-
-  private
-
-  def req_class(membership_certs, type_def)
-    return 'danger'  if type_def.required? && membership_certs.empty?
-    exp_dates = membership_certs.map {|cert| cert.user_cert.expires_at}
-    return 'danger'  if type_def.required? && exp_dates.any? {|date| date.blank? }
-    return ''        if exp_dates.empty?
-    #return "danger"  if type_def.expirable? && exp_dates.any? {|date| date.blank? }
-    return 'danger'  if exp_dates.any? {|date| date.present? && date < Time.now }
-    return 'warning' if exp_dates.any? {|date| date.present? && date < Time.now + 1.month  }
-    return 'info'    if exp_dates.any? {|date| date.present? && date < Time.now + 3.months }
-    return 'success' if exp_dates.all? {|date| date.present? && date > Time.now }
-    ''
-  end
-
-end
+# class MemberCertDecorator < Draper::Decorator
+#   delegate_all
+#
+#   def cert_cells
+#     types = h.current_team.qual_ctypes
+#     return ''
+#     # types.to_a.reduce("") do |acc, obj|
+#       #certs = membership_certs.where(typ: obj.id)
+#       # certs = membership_certs.where('typ ilike ?', obj.id)
+#       # acc + cert_cell(certs, obj)
+#     # end
+#   end
+#
+#   # def cert_cell(certs, type_def)
+#   #   val   = req_val(certs)
+#   #   klas  = req_class(certs, type_def)
+#   #   "<td class='#{klas}'><small>#{val}</small></td>"
+#   # end
+#
+#   def req_val(certs)
+#     return '' if certs.blank?
+#     aster  = certs.length > 1 ? ' *' : ''
+#     req_link(certs.first, aster)
+#   end
+#
+#   def req_link(cert, aster)
+#     label = "#{cert.title}#{aster}"
+#     link  = cert.attachment_file_name.present? ? cert.attachment.url : ''
+#     return label if link.empty?
+#     "<a href='#{link}'>#{label}</a>"
+#   end
+#
+#   private
+#
+#   def req_class(membership_certs, type_def)
+#     return 'danger'  if type_def.required? && membership_certs.empty?
+#     exp_dates = membership_certs.map {|cert| cert.user_cert.expires_at}
+#     return 'danger'  if type_def.required? && exp_dates.any? {|date| date.blank? }
+#     return ''        if exp_dates.empty?
+#     #return "danger"  if type_def.expirable? && exp_dates.any? {|date| date.blank? }
+#     return 'danger'  if exp_dates.any? {|date| date.present? && date < Time.now }
+#     return 'warning' if exp_dates.any? {|date| date.present? && date < Time.now + 1.month  }
+#     return 'info'    if exp_dates.any? {|date| date.present? && date < Time.now + 3.months }
+#     return 'success' if exp_dates.all? {|date| date.present? && date > Time.now }
+#     ''
+#   end
+#
+# end

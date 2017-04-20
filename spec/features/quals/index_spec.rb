09e660011ba48b8c5d7a1a2547822c020ab628b8
andyl <andy@r210.com>
2017-04-20 10:10:42 -0700

    Fix qual spec bug

--- spec/features/quals/index_spec.rb
+++ spec/features/quals/index_spec.rb
@@ -2,20 +2,14 @@ require 'rails_helper'
 
 feature 'quals#index'  do
 
-  let(:orgn) { FG.create(:org)                                            }
-  let(:team) { FG.create(:team, org_id: orgn.id)                          }
-  let(:pagr) { Pgr.create(team_id: team.id)                               }
-  let(:usr1) { FG.create(:user_with_phone_and_email)                      }
-  let(:usr2) { FG.create(:user_with_phone_and_email)                      }
-  let(:mem1) { FG.create(:membership, team_id: team.id, user_id: usr1.id) }
-  let(:mem2) { FG.create(:membership, team_id: team.id, user_id: usr2.id) }
+  include_context "Integration Environment"
 
   describe 'page rendering' do
 
-    before(:each) { set_feature_host(team, orgn) }
+    before(:each) { set_feature_host(team1) }
 
     it 'renders /quals' do
-      capy_create_member_and_login team
+      capy_create_member_and_login team1
       visit '/quals'
       expect(current_path).to eq('/quals')
       expect(page.status_code).to be 200

ca667318f0726354fd2524f77657982296644a2b
andyl <andy@r210.com>
2017-07-01 14:46:23 -0700

    Remove comment

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -3,7 +3,7 @@ branch = `git rev-parse --abbrev-ref HEAD`.chomp
 puts "CURRENT BRANCH <#{branch}>"
 puts ' TARGET ENVIRONMENT: VAGRANT '.center(70, '-')
 
-# abort "EXITING: VAGRANT CAP ONLY RUNS IN DEV BRANCH" unless branch == "dev"
+abort "EXITING: VAGRANT CAP ONLY RUNS IN DEV BRANCH" unless branch == "dev"
 
 set :stage,     :vagrant
 set :user,      'deploy'

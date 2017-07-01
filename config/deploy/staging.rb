a5a27733787de7be5e3b9f841be9ab905854b80b
andyl <andy@r210.com>
2017-07-01 14:45:25 -0700

    Rename vagrant to staging

---
+++ config/deploy/staging.rb
@@ -0,0 +1,17 @@
+# only run in dev branch
+branch = `git rev-parse --abbrev-ref HEAD`.chomp
+puts "CURRENT BRANCH <#{branch}>"
+puts ' TARGET ENVIRONMENT: VAGRANT '.center(70, '-')
+
+# abort "EXITING: VAGRANT CAP ONLY RUNS IN DEV BRANCH" unless branch == "dev"
+
+set :stage,     :vagrant
+set :user,      'deploy'
+
+set :branch,    'dev'
+
+set :rails_env, 'staging'
+
+role :app, ['deploy@smso.vbox']
+role :db,  ['deploy@smso.vbox']
+role :web, ['deploy@smso.vbox']

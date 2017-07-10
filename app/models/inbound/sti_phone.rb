4296fc1eee903cf7625f5d7ac869804929bdc2a9
andyl <andy@r210.com>
2017-07-10 10:57:26 -0700

    Add sti_phone class

---
+++ app/models/inbound/sti_phone.rb
@@ -0,0 +1,26 @@
+class Inbound::StiPhone < Inbound
+
+  # KILLME - placeholder to handle database instances
+
+end
+
+# == Schema Information
+#
+# Table name: inbounds
+#
+#  id               :integer          not null, primary key
+#  team_id          :integer
+#  pgr_dialog_id    :integer
+#  type             :string
+#  proxy            :string
+#  subject          :string
+#  fm               :string
+#  to               :string           default("{}"), is an Array
+#  headers          :hstore           default("")
+#  text             :text
+#  destination_type :string
+#  destination_id   :integer
+#  xfields          :hstore           default("")
+#  created_at       :datetime
+#  updated_at       :datetime
+#

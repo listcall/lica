d573853c0ec89858243450dbe273f9e7cfb05871
andyl <andy@r210.com>
2017-07-29 09:36:39 -0700

    Revise spec migrations

--- db/migrate/20170708143611_table_rename.rb
+++ db/migrate/20170708143611_table_rename.rb
@@ -1,6 +1,6 @@
 class TableRename < ActiveRecord::Migration[5.1]
   def change
-    create_table 'cert_exhibits' do |t|
+    create_table 'cert_evidence' do |t|
       t.integer  'user_id'
       t.string   'comment'                 # for documentation
       t.string   'link'                    # for documentation
@@ -15,14 +15,31 @@ class TableRename < ActiveRecord::Migration[5.1]
       t.hstore   'xfields', default: {}    # for extended fields
       t.timestamps
     end
-    add_index :cert_exhibits, :user_id
+    add_index :cert_evidence, :user_id
+
+    create_table 'cert_specs' do |t|
+      t.integer 'team_id'
+      t.string  'name'
+      t.string  'rname'
+      t.boolean 'expirable'   , default: true
+      t.boolean 'commentable' , default: true
+      t.hstore  'xfields' , default: {}
+      t.text    'ev_types', default: [], array: true
+      t.timestamps
+    end
+    add_index :cert_specs, :team_id
+    add_index :cert_specs, :rname
+    add_index :cert_specs, :expirable
+    add_index :cert_specs, :commentable
+    add_index :cert_specs, :xfields , :using => :gin
+    add_index :cert_specs, :ev_types, :using => :gin
 
     create_table 'cert_assignments' do |t|
       t.integer  'membership_id'
       t.integer  'cert_spec_id'
-      t.integer  'cert_exhibit_id'
+      t.integer  'cert_evidence_id'
       t.string   'title'
-      t.integer  'position'     # scoped by membership and type
+      t.integer  'position'              # scoped by membership and spec
       t.string   'status'
       t.string   'ev_type'
       t.integer  'reviewer_id'
@@ -34,7 +51,7 @@ class TableRename < ActiveRecord::Migration[5.1]
     end
     add_index :cert_assignments, :membership_id
     add_index :cert_assignments, :cert_spec_id
-    add_index :cert_assignments, :cert_exhibit_id
+    add_index :cert_assignments, :cert_evidence_id
     add_index :cert_assignments, :title
     add_index :cert_assignments, :position
     add_index :cert_assignments, :external_id
@@ -42,59 +59,32 @@ class TableRename < ActiveRecord::Migration[5.1]
     add_index :cert_assignments, :ev_type
     add_index :cert_assignments, :xfields, :using => :gin
 
-    create_table 'cert_specs' do |t|
-      t.integer 'team_id'
-      t.integer 'cert_role_id'
-      t.string  'name'
-      t.string  'rname'
-      t.boolean 'expirable'   , default: true
-      t.boolean 'commentable' , default: true
-      t.hstore  'xfields' , default: {}
-      t.text    'ev_types', default: [], array: true
-      t.integer 'position'
-      t.timestamps
-    end
-    add_index :cert_specs, :team_id
-    add_index :cert_specs, :cert_role_id
-    add_index :cert_specs, :rname
-    add_index :cert_specs, :expirable
-    add_index :cert_specs, :commentable
-    add_index :cert_specs, :xfields , :using => :gin
-    add_index :cert_specs, :ev_types, :using => :gin
-    add_index :cert_specs, :position
-
     create_table 'cert_groups' do |t|
       t.integer 'team_id'
+      t.integer 'position'
       t.string  'name'
       t.hstore  'xfields', default: {}
       t.jsonb   'jfields', default: {}
       t.timestamps
     end
+    add_index :cert_groups, :position
     add_index :cert_groups, :xfields , :using => :gin
     add_index :cert_groups, :jfields , :using => :gin
     add_index :cert_groups, :team_id
 
-    create_table :access_roles do |t|
-      t.integer 'team_id'
+    create_table 'cert_spec_groups' do |t|
       t.integer 'cert_spec_id'
-      t.string  'name'
-      t.string  'acronym'
-      t.string  'description'
-      t.integer 'sort_key'
-      t.hstore  'xfields', default: {}
-      t.jsonb   'jfields', default: {}
-      t.timestamps
+      t.integer 'cert_group_id'
+      t.integer 'position'                      # scoped by cert_group and cert_spec
     end
-    add_index :access_roles, :team_id
-    add_index :access_roles, :cert_spec_id
-    add_index :access_roles, :acronym
-    add_index :access_roles, :sort_key
-    add_index :access_roles, :xfields, :using => :gin
-    add_index :access_roles, :jfields, :using => :gin
+    add_index :cert_spec_groups, :cert_spec_id
+    add_index :cert_spec_groups, :cert_group_id
+    add_index :cert_spec_groups, :position
 
-    create_table :access_permissions do |t|
-      t.integer 'access_role_id'
-      t.string 'label'
+    create_table :cert_permissions do |t|
+      t.integer 'cert_spec_id'
+      t.string  'label'
     end
+    add_index :cert_permissions, :cert_spec_id
   end
 end
